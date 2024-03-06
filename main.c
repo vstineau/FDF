d5b1d8034aa698eeee2b4d237e37040e3b788c62
Vincent Stineau <vstineau@bess-f2r1s10.clusters.42paris.fr>
2024-03-06 17:50:50 +0100

    parsing malloc protection

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: vstineau <vstineau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 11:35:02 by vstineau          #+#    #+#             */
-/*   Updated: 2024/03/06 17:40:29 by vstineau         ###   ########.fr       */
+/*   Updated: 2024/03/06 17:50:28 by vstineau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int	main(int argc, char *argv[])
 		return (1);
 	v.map = parse(argv[1], &v);
 	v.mlx = mlx_init();
+	if (!v.mlx)
+		return (1);
 	v.win = mlx_new_window(v.mlx, WIN_WIDTH, WIN_HEIGHT, "FDF");
 	mlx_hook(v.win, KeyPress, KeyPressMask, window_action, &v);
 	mlx_loop(v.mlx);
